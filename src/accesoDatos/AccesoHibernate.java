a741a381cb10e51e58857eeb73c72d485fff4079
David <davidangulocorcuera@gmail.com>
2018-10-25 19:16:42 +0200

    obtenerDispensadores and depositos do it!

--- src/accesoDatos/AccesoHibernate.java
+++ src/accesoDatos/AccesoHibernate.java
@@ -2,9 +2,11 @@ package accesoDatos;
 
 import logicaRefrescos.Deposito;
 import logicaRefrescos.Dispensador;
+import org.hibernate.Query;
 import org.hibernate.Session;
-
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 public class AccesoHibernate implements I_Acceso_Datos {
     Session session;
@@ -18,12 +20,40 @@ public class AccesoHibernate implements I_Acceso_Datos {
     }
     @Override
     public HashMap<Integer, Deposito> obtenerDepositos() {
-        return null;
+        HashMap<Integer,Deposito>  hm_depositos = new HashMap<Integer,Deposito>();
+        Deposito deposito;
+        Query q = session.createQuery("select e from Deposito e");
+        List results = q.list();
+
+        Iterator depositosIterator = results.iterator();
+
+        while (depositosIterator.hasNext()) {
+             deposito = (Deposito) depositosIterator.next();
+            hm_depositos.put(deposito.getValor(), deposito);
+
+        }
+
+
+        return hm_depositos;
     }
 
     @Override
     public HashMap<String, Dispensador> obtenerDispensadores() {
-        return null;
+        HashMap<String,Dispensador>  hm_dispensadores = new HashMap<String, Dispensador>();
+        Dispensador dispensador;
+        Query q = session.createQuery("select e from Dispensador e");
+        List results = q.list();
+
+        Iterator dispensadoresIterator = results.iterator();
+
+        while (dispensadoresIterator.hasNext()) {
+            dispensador = (Dispensador) dispensadoresIterator.next();
+            hm_dispensadores.put(dispensador.getClave(), dispensador);
+
+        }
+
+
+        return hm_dispensadores;
     }
 
     @Override
