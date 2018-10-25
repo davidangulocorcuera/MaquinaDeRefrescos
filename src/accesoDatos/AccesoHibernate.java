de25bc523ed73497cc77249bf4e196c766f2ff19
David <davidangulocorcuera@gmail.com>
2018-10-25 14:33:34 +0200

    hibernate added!

--- src/accesoDatos/AccesoHibernate.java
+++ src/accesoDatos/AccesoHibernate.java
@@ -2,10 +2,20 @@ package accesoDatos;
 
 import logicaRefrescos.Deposito;
 import logicaRefrescos.Dispensador;
+import org.hibernate.Session;
 
 import java.util.HashMap;
 
 public class AccesoHibernate implements I_Acceso_Datos {
+    Session session;
+
+    public AccesoHibernate() {
+
+        HibernateUtil util = new HibernateUtil();
+
+        session = util.getSessionFactory().openSession();
+
+    }
     @Override
     public HashMap<Integer, Deposito> obtenerDepositos() {
         return null;
